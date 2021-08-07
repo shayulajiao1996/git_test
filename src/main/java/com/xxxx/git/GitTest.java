08340d6d7ca476f21275392e105e5838a392b5ed
tom_glb <goodMorning_glb@163.com>
2021-08-07 17:53:03 +0800

    first commit

---
+++ src/main/java/com/xxxx/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.xxxx.git;
+
+/**
+ * @author lizhe
+ * @create 2021-08-07 17:31
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
